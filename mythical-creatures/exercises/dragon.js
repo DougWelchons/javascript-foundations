627bd2d3151ea63695200fd8cfd8cd7091c1e2fb
DougWelchons <doug.welchons@gmail.com>
2021-05-04 18:06:44 -0600

    add Dragon passing tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider)
+  {
+    this.name = name
+    this.rider = rider
+    this.hungry = true
+    this.foodEaten = 0
+  }
 
+  greet()
+  {
+    return 'Hi, ' + this.rider + '!'
+  }
+
+  eat()
+  {
+    this.foodEaten += 1
+    this.hungry = this.foodEaten < 3
+  }
+}
+
+
+module.exports = Dragon
